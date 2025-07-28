17042b1ba37a87386aa390b85237e88f23b20ad8
Tatyana Lev <delovoiy86@gmail.com>
2025-07-28 20:19:33 +0300

    corrected

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ProjectList = ({ projects }) => {
   return (
     <div className="projects">
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import cn from "classnames";
 
 const Toolbar = ({ filters, selected, onSelectFilter }) => {
@@ -7,17 +6,12 @@ const Toolbar = ({ filters, selected, onSelectFilter }) => {
       <ul className="toolbar-list">
         {filters.map((filter, idx) => (
           <li className="toolbar-list-elem" key={idx}>
-            {/* 
-              Кнопка фильтра.
-              Если этот filter совпадает с выбранным (selected), 
-              к классу добавится 'btn-category_selected'
-            */}
             <button
               className={cn(
                 "btn-category",
                 filter === selected && "btn-category_selected"
               )}
-              onClick={() => onSelectFilter(filter)} // при клике передаём filter наверх
+              onClick={() => onSelectFilter(filter)}
             >
               {filter}
             </button>
